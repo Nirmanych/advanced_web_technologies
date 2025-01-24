7c5f209dd6f8a427cefab2f026834210bfb9e2d3
Nirmanych <nursnirma@gmail.com>
2025-01-24 18:53:54 +0500

    Step 5: Integrating JSX.

--- src/index.js
+++ src/index.js
@@ -2,30 +2,50 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 const TodoList = () => {
-  return (
-        <ul>
-            <li>Install React</li>
-            <li>Study React</li>
-            <li>Use React</li>
-            <li>Build React App</li>
-        </ul>
-    );
+    const items = ['Install React', 'Study Reac', 'Use React', 'Build React App'];
+    return (
+            <ul>
+                <li>{items[0]}</li>
+                <li>{items[1]}</li>
+                <li>{items[2]}</li>
+                <li>{items[3]}</li>
+            </ul>
+        );
   };
+
   const AppHeader = () =>{
       return <h1>My Todo List</h1>;
   };
+
   const SearchPanel = () => {
-      return <input placeholder="search" />;
+        const searchText = 'Type here to search';
+        const searchStyle = {
+            fontSize: '20px'
+        };
+
+        return <input
+            style = {searchStyle}
+            placeholder= {searchText}
+            disabled={true} />;
   }
+
   const App = () => {
-      return (
-          <div>
-              <AppHeader />
-              <SearchPanel />
-              <TodoList/>
-          </div>
-      );
+        const value = '<script>alert ("")</script>';
+        // const isLoggedIn = false;
+        // const loginBox = <span>Log in to the system</span>;
+        // const welcomeBox = <span>Welcome Back</span>;
+
+        return (
+            <div>
+                {/*{ isLoggedIn ? null : loginBox }*/}
+                { value }
+                <AppHeader />
+                <SearchPanel />
+                <TodoList/>
+            </div>
+        );
   }
+
   // const element = <App />;
   ReactDOM.render(<App />,        
     document.getElementById('root'));
