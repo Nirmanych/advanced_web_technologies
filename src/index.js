e4546c3693bc4ee18fa0227f58eef707669ff0a6
Nirmanych <nursnirma@gmail.com>
2025-01-24 17:29:39 +0500

    Step 3.5: Creating ToDo list basics.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, Nursultan!');
+import React from "react";
+import ReactDOM from 'react-dom';
+
+const element = (
+    <div>
+        <h1>My ToDo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Install React</li>
+            <li>Study React</li>
+            <li>Use React</li>
+            <li>Build React App</li>
+        </ul>
+    </div>
+    );
+    ReactDOM.render(element,
+        document.getElementById('root'));
