926048e2fa773a263c3ed2dd28e347c83d665c79
Nirmanych <nursnirma@gmail.com>
2025-02-18 23:02:00 +0500

    Step 35: Adding task filtering. Implementing functionality to filter buttons.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ export  default  class  App extends Component {
                 this.createTodoItem('Make Awesome App'),
                 this.createTodoItem('Have a lunch')
             ],
-            term: ''
+            term: '',
+            filter: 'all'
        };
 
 
@@ -92,6 +93,10 @@ export  default  class  App extends Component {
         this.setState({term});
     };
 
+    onFilterChange = (filter) => {
+        this.setState({filter});
+    };
+
     search(items, term) {
         if (term.length === 0) {
             return items;
@@ -103,10 +108,24 @@ export  default  class  App extends Component {
         });
     }
 
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    }
+
     render() {
 
-        const { todoData, term } = this.state;
-        const visibleItems = this.search(todoData, term);
+        const { todoData, term, filter } = this.state;
+        const visibleItems = this.filter(
+            this.search(todoData, term), filter);
         const doneCount = todoData.filter((element) => element.done).length;
         const todoCount = todoData.length - doneCount;
 
@@ -115,8 +134,10 @@ export  default  class  App extends Component {
                     <AppHeader toDo={todoCount} done={doneCount} />
                     <div className="top-panel d-flex">
                     <SearchPanel
-                    onSearchChange = {this.onSearchChange} />
-                    <ItemStatusFilter />
+                        onSearchChange = {this.onSearchChange} />
+                    <ItemStatusFilter
+                        filter = {filter}
+                        onFilterChange = {this.onFilterChange} />
                 </div>
                 <TodoList todos={ visibleItems  }
                     onDeleted = { this.deleteItem }
